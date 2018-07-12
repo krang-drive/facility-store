9e6a432b2244dc03585e00dc48153126552f18bd
jmh9014 <johnh0618@yahoo.com>
2018-07-12 17:12:21 -0400

    feat: Add post and get requests for facility

--- server.js
+++ server.js
@@ -1,8 +1,72 @@
+//----------------------------------------------------------------------------//
+//                               Imports & Constants                          //
+//----------------------------------------------------------------------------//
+
+//Importing express module
 const express = require('express');
+
+//Setting express to a constant
 const app = express();
 
-app.get('/', (req, res) => res.send('Hello, World!'));
+//Importing mongoose module
+var mongoose = require('mongoose');
 
-app.listen(8080, () => console.log('App listening on port (krang) 8080!'));
+//Set up default mongoose connection
+var mongoDB = 'mongodb://localhost/facility-store-db';
 
-// KRANG
+//Connecting mongoose to MongoDB
+mongoose.connect(mongoDB);
+
+//Setting the schema to use mongoose.
+var Schema = mongoose.Schema;
+
+//----------------------------------------------------------------------------//
+//                               Schema & Model Creation                      //
+//----------------------------------------------------------------------------//
+
+//Creating a facilitySchema.
+var facilitySchema = new Schema({
+
+    facilityID: String,
+    location: String
+
+})
+
+//Create a model that uses the schema.
+var Facility = mongoose.model('Facility', facilitySchema);
+
+//Make this available to Node app users.
+module.exports = facility;
+
+//----------------------------------------------------------------------------//
+//                               Function Calls                               //
+//----------------------------------------------------------------------------//
+
+app.get('/facility', function (req, res)) {
+
+  FacilityModel.find({facilityID: req.query.facilityID}, function (err, facility){
+
+    res.send(facility);
+
+  });
+
+});
+
+//----------------------------------------------------------------------------//
+
+app.post('/form', function(req, res){
+
+    facility.findOneAndUpdate({facilityID: req.body.facilityID}, req.body , {upsert:true})
+
+    //Writing out the post information to console.
+    console.log('POST -> facilityID: ' + req.body.facilityID + ', location: ' + req.body.location);
+
+});
+
+//----------------------------------------------------------------------------//
+
+app.listen(8080, function() {
+
+  console.log('Listening on port 8080!')
+
+});
